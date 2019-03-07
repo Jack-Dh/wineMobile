b9bd5ac418dcec78452df1133587923dba2995c2
Zmyhh <897268593@QQ.com>
2019-03-07 15:03:35 +0800

    酒品管理系统，移动端页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div style="width: 100%;height: 40%">
+    <img style="width: 100%;height: auto" :src="parameter">
+  </div>
+
+
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-};
+
+  export default {
+    name: 'home',
+    data() {
+      return {
+        parameter: '',//图片地址
+      }
+    },
+    methods:{
+      imgLoad(){
+        let url = window.location.href
+        let a = url.indexOf('=')
+        this.parameter = url.substring(a + 1)
+
+        this.$axios.get('http://qrcode.jiajiachuang.cn/qrcode/qrcode/selectById', {
+          params: {
+            id: this.parameter
+          }
+        }).then(res => {
+          this.parameter = `http://${res.data.data.imageUrl}`
+        })
+      }
+    },
+    created: function () {
+      this.imgLoad()
+    }
+  };
 </script>
